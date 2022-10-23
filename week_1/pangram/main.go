031fdf1eececfa35d19dab61ebdbce10349d0e0c
Serhii Chernysh <Serhii.Chernysh@Sophos.com>
2022-10-23 23:08:33 +0300

    Week_1 homework Pangrama

--- week_1/pangram/main.go
+++ week_1/pangram/main.go
@@ -1,10 +1,32 @@
 package pangram
 
+import (
+	"strings"
+)
+
 // IsPangram checks if given string is a pangram
 func IsPangram(s string) bool {
 	// Тут має бути рішення
 	// написавши код - необхідно запустити тести
 	// Ці коментарі можна видаляти
 	// !ВАЖЛИВО - не забудьте виправити return
-	return false
+
+	// Англійський алфавіт (довжина 26 символів)
+	alphabet := string("abcdefghijklmnopqrstuvwxyz")
+	// Кількість надходжень співпадінь при порівнянні
+	accumulator := 0
+
+	/* Якщо символ в адфавіті співпадає з символом в строці-аргументі,
+	то сбільшуємо інкрементуємо accumulator
+	*/
+	for i := 0; i < len(alphabet); i++ {
+		for j := 0; j < len(s); j++ {
+			if string(alphabet[i]) == strings.ToLower(string(s[j])) {
+				accumulator++
+				break
+			}
+		}
+	}
+	// true = pangrama
+	return (accumulator == len(alphabet))
 }
