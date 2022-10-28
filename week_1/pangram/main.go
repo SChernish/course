20871d4f657a4f523c65aef500bb8e75ed75bfe0
Serhii Chernysh <Serhii.Chernysh@Sophos.com>
2022-10-28 17:43:10 +0300

    Final refactoring pangrama task

--- week_1/pangram/main.go
+++ week_1/pangram/main.go
@@ -5,11 +5,32 @@ import (
 )
 
 /* 2 Additional solutions
-1. Compare unicode
-2. Use packet string
+1. Compare string as UTF
+2. Compare unicode
+3. Compare using packet string
 */
 
-// Solution 1
+//Solution 1
+/*
+func IsPangram(s string) bool {
+	var (
+		alphabet    = []rune("abcdefghijklmnopqrstuvwxyz")
+		str         = strings.ToLower(string(s))
+		accumulator = 0
+	)
+	for i := 0; i < len([]rune(alphabet)); i++ {
+		for j := 0; j < len([]rune(str)); j++ {
+			if []rune(alphabet)[i] == []rune(str)[j] {
+				accumulator++
+				break
+			}
+		}
+	}
+	return (accumulator == len(alphabet))
+}
+*/
+
+// Solution 2
 /*
 func IsPangram(s string) bool {
 
@@ -32,7 +53,7 @@ func IsPangram(s string) bool {
 }
 */
 
-// Solution 2
+// Solution 3
 func IsPangram(s string) bool {
 	var (
 		alphabet  = []rune("abcdefghijklmnopqrstuvwxyz")
